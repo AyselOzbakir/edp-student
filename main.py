3b9cfeba0c1956619e093ca926edc223de51b7c3
AyselOzbakir <150921363+AyselOzbakir@users.noreply.github.com>
2025-01-17 11:37:20 +0100

    main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from appointment_confirmation_event import AppointmentConfirmationEvent
 
 event_queue = []
 
-# Example Usage
 student1 = Student("Piotr1", "Brudny", '1.02.1984', 'Ankara', '5435345345', 'ED4234323', event_queue)
 polish_embassy = Embassy('Polish Embassy', 'Ankara, Harika 10', '343242344', 'polishembassy@gov.tr', event_queue)
 
@@ -13,10 +12,10 @@ polish_embassy = Embassy('Polish Embassy', 'Ankara, Harika 10', '343242344', 'po
 student1.ask_for_embassy_appointment('10.12.2024')
 
 
-while event_queue: # it runs as long as there is an event in the list
-    event = event_queue.pop(0) # takes the event from the top
+while event_queue: 
+    event = event_queue.pop(0) 
 
-    # matches events with the handler
+
     if isinstance(event, EmbassyAppointmentRequestEvent):
         polish_embassy.handle_appointment_request(event) 
     elif isinstance(event, AppointmentConfirmationEvent):
